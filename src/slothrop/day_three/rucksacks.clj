3c19592565110a93b1cf57eb09286800fb081488
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-12-03 19:13:24 -0700

    cleaned up day three solution

--- src/slothrop/day_three/rucksacks.clj
+++ src/slothrop/day_three/rucksacks.clj
@@ -1,19 +1,16 @@
 (ns slothrop.day-three.rucksacks
-  (:require [clojure.java.io :as io] 
+  (:require [clojure.java.io :as io]
             [clojure.set :refer [intersection]]))
 
 (def lower (zipmap (map char (range 97 123)) (range 1 27)))
 (def upper (zipmap (map char (range 65 91)) (range 27 53)))
 
-(with-open [rdr (-> "public/puzzle_inputs/day_three.txt"
-                    io/resource
-                    io/reader)]
-  (->> rdr
-       line-seq
-       (partition 3) ; pt 2
-       #_(map #(split-at (/ (count %) 2) %)) ; pt 1
-       (map (fn [[s1 s2 s3]] (intersection (set s1) (set s2) (set s3))))
-       (map #(if (Character/isUpperCase (first %)) 
-               (get upper (first %)) 
-               (get lower (first %))))
-       (apply +)))
+(with-open [rdr (-> "public/puzzle_inputs/day_three.txt" io/resource io/reader)]
+  (letfn [(setify [s] (apply intersection (map set s)))
+          (char->number [c] (or (get lower c) (get upper c)))]
+    (->> rdr
+         line-seq
+         (partition 3) ; pt 2
+         #_(map #(split-at (/ (count %) 2) %)) ; pt 1
+         (mapv (comp char->number first setify))
+         (apply +))))
