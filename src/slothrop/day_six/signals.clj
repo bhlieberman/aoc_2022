555232d690e170fe957b6b8a1017f6da3c758ef4
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-12-06 08:51:32 -0700

    removed superfluous apply str

--- src/slothrop/day_six/signals.clj
+++ src/slothrop/day_six/signals.clj
@@ -4,8 +4,8 @@
 (def input (-> "public/puzzle_inputs/day_six.txt" io/resource slurp))
 
 (defn process-data [n s]
-  (let [packets (map (partial apply str) (partition n 1 s))
-        unique-packets (map (comp (partial apply str) distinct) packets)
+  (let [packets (partition n 1 s)
+        unique-packets (map distinct packets)
         nums (map #(if (= %1 %2) %3 nil) packets unique-packets (range))]
     (->> nums (filter some?) first (+ n))))
 
